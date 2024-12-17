a8b11af290344fb9c7e54c753e5558cb99015bb8
Risdawgg <158297290+Risdawgg@users.noreply.github.com>
2024-12-17 13:27:37 +0530

    Add files via upload

---
+++ Bg-color-changer/index.js
@@ -0,0 +1,15 @@
+function red() {
+  document.body.style.backgroundColor = 'red'
+}
+function yellow() {
+  document.body.style.backgroundColor = 'yellow'
+}
+function green() {
+  document.body.style.backgroundColor = 'green'
+}
+function pink() {
+  document.body.style.backgroundColor = 'pink'
+}
+function reset() {
+  document.body.style.backgroundColor = 'white'
+}
